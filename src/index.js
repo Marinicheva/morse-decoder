1c5a4133285db13ae86ea1b32da280e1dc46051b
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-01-28 15:10:50 +0300

    feat: add script for resolve task

--- src/index.js
+++ src/index.js
@@ -39,8 +39,47 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let input = expr.split('');
+
+    let sym = '',
+        arr = [],
+        count = 0,
+        space = "**********",
+        str = '',
+        dot = '10',
+        dash = '11';
+
+
+    for (let item of input) {
+        sym += item;
+        count++;
+
+        if (count === 10) {
+            arr.push(sym);
+            sym = '';
+            count = 0;
+        }
+    }
+
+    arr = arr.map(item => {
+        let index = item.indexOf('1');
+        if (index >= 0) {
+            item = item.slice(index).split('10').join('.').split('11').join('-');
+        }
+        return item;
+    });
+
+    arr.forEach(item => {
+        if (item === space) {
+            str += ' ';
+        } else {
+            str += MORSE_TABLE[item];
+        }
+    });
+
+    return str;
 }
 
 module.exports = {
     decode
-}
+};
